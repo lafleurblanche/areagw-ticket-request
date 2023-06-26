5160cc530bb45b5013c4e3fc9c518bb4529e8653
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-26 09:05:19 +0900

    20230626 #29 駅名コード誤り修正

--- areagw-ticket-request/src/features/RouteSelect/ToStaList/AreaGW01ToStaList01.ts
+++ areagw-ticket-request/src/features/RouteSelect/ToStaList/AreaGW01ToStaList01.ts
@@ -1,27 +1,27 @@
 const AreaGW01FromStaList01 = [
   {
     id: 1,
-    staCode: 'GW0100',
+    staCode: 'GW0101',
     staName: '茆川'
   },
   {
     id: 2,
-    staCode: 'GW0101',
+    staCode: 'GW0102',
     staName: '上茆川'
   },
   {
     id: 3,
-    staCode: 'GW0102',
+    staCode: 'GW0103',
     staName: '書石'
   },
   {
     id: 4,
-    staCode: 'GW0103',
+    staCode: 'GW0104',
     staName: '宴谷'
   },
   {
     id: 5,
-    staCode: 'GW0104',
+    staCode: 'GW0105',
     staName: '上宴谷'
   },
 ];
